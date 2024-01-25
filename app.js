458693584baa1df764264985a0283d8ae3551c50
Courtney <cpride92@gmail.com>
2024-01-25 16:54:12 -0600

    quick

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
+
+// set the view engine to ejs
+
+let path = requre('path'); 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// use res.render to load up an ejs view file
+
+//type in result from the enneagram test
+let myTypeServer = "";
 
 app.get('/', function (req, res) {
- 
-    res.send('Hello World from Express')
 
+    res.render('index', {
+
+        myTypeClient: myTypeServer
+    });
+})    
+
+app.get('/', function (req, res) {
+  
+    res.send('Hello World from Express ')
 })
 
-app.listen(3000)
+// app.listen(3000)
+
+app.listen(port, () => {
+    
+  console.log(`mike app listening on port ${port}`)
+})
