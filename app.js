73af7f96585345e6220a6380991dc0204f027297
Courtney <66399339+cj89112@users.noreply.github.com>
2024-01-25 16:05:01 -0600

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
 
 app.get('/', function (req, res) {
- 
-    res.send('Hello World from Express')
-
+  
+    res.send('Hello World from Express ')
 })
 
-app.listen(3000)
+// app.listen(3000)
+
+app.listen(port, () => {
+  console.log(`mike app listening on port ${port}`)
+})
